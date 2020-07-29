edc553c105586c0583584f08a169dc624d976df6
gizems <saricicek.gizem07@gmail.com>
2020-07-29 10:35:53 +0300

    v2 migration da id leri değiştirdim

--- src/main/resources/db/migration/V2__1_migration.sql
+++ src/main/resources/db/migration/V2__1_migration.sql
@@ -1,3 +1,3 @@
 
- INSERT INTO  stajyer.tb_kullanici ( id,  kullanici_adi,  sifre)  VALUES ( 11, 'admin2', '1');
- INSERT INTO  stajyer.tb_kullanici ( id,  kullanici_adi,  sifre)  VALUES ( 22, 'stajyer2', '2');
+ INSERT INTO  stajyer.tb_kullanici ( id,  kullanici_adi,  sifre)  VALUES ( 10, 'admin2', '1');
+ INSERT INTO  stajyer.tb_kullanici ( id,  kullanici_adi,  sifre)  VALUES ( 20, 'stajyer2', '2');
